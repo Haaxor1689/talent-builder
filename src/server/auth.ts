bdabb74c5f4562dc714edf2a1d77a820729c006e
Haaxor1689 <betkomaros@gmail.com>
2024-05-13 15:21:07 +0200

    Caching fixes

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -153,4 +153,4 @@ export const authOptions: NextAuthOptions = {
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = cache(() => getServerSession(authOptions));
+export const getServerAuthSession = () => getServerSession(authOptions);
