1839027a5803b8d1c6fb59061e234e9ee3cb62ba
Haaxor1689 <betkomaros@gmail.com>
2026-02-09 00:26:10 +0100

    Footer update

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,14 +3,30 @@ import { Bug } from 'lucide-react';
 import TextButton from '~/components/styled/TextButton';
 
 const Footer = async () => (
-	<footer className="text-blue-gray flex max-w-7xl items-center justify-center gap-1 p-2 md:px-6 md:py-2 xl:mx-auto xl:w-full">
-		<p className="text-blue-gray pr-2 text-sm">Created by Haaxor1689</p>•
+	<footer className="text-blue-gray flex max-w-7xl flex-wrap items-center justify-center gap-3 p-2 md:px-6 md:py-2 xl:mx-auto xl:w-full">
+		<p className="text-blue-gray text-sm">Created by Haaxor1689</p>•
+		<TextButton
+			type="link"
+			icon={() => (
+				// eslint-disable-next-line @next/next/no-img-element
+				<img
+					src="https://storage.ko-fi.com/cdn/logomarkLogo.png"
+					alt="Ko-fi logo"
+					className="h-4 shrink-0 pr-1"
+				/>
+			)}
+			href="https://ko-fi.com/haaxor1689"
+			className="-m-2"
+		>
+			Support me
+		</TextButton>
+		•
 		<TextButton
 			type="link"
 			href="https://discord.gg/pDeTHQH99B"
 			icon={Bug}
 			iconSize={18}
-			className="text-blue-gray shrink-0 text-sm"
+			className="  text-sm text-orange text-orange"
 		>
 			Report a bug
 		</TextButton>
@@ -18,7 +34,7 @@ const Footer = async () => (
 		<TextButton
 			type="link"
 			href="https://haaxor1689.dev/privacy-policy"
-			className="text-blue-gray shrink-0 text-sm"
+			className="text-blue-gray -m-2 shrink-0 text-sm"
 		>
 			Privacy Policy
 		</TextButton>
