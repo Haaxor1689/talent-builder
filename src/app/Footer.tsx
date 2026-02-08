393429ea753758e7f8647199e9b75283e59c3ae6
Haaxor1689 <betkomaros@gmail.com>
2026-02-08 23:39:46 +0100

    Update footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,17 +2,25 @@ import { Bug } from 'lucide-react';
 
 import TextButton from '~/components/styled/TextButton';
 
-const Footer = () => (
-	<footer className="flex max-w-screen-xl items-center justify-center gap-3 p-2 md:px-6 md:py-2 xl:mx-auto xl:w-full">
-		<p className="text-sm text-blueGray">Created by Haaxor1689</p>
+const Footer = async () => (
+	<footer className="text-blue-gray flex max-w-7xl items-center justify-center gap-1 p-2 md:px-6 md:py-2 xl:mx-auto xl:w-full">
+		<p className="text-blue-gray pr-2 text-sm">Created by Haaxor1689</p>•
 		<TextButton
 			type="link"
-			href="https://github.com/Haaxor1689/talent-builder/issues"
+			href="https://discord.gg/pDeTHQH99B"
 			icon={Bug}
 			iconSize={18}
-			className="shrink-0 text-sm text-blueGray"
+			className="text-blue-gray shrink-0 text-sm"
 		>
 			Report a bug
+		</TextButton>
+		•
+		<TextButton
+			type="link"
+			href="https://haaxor1689.dev/privacy-policy"
+			className="text-blue-gray shrink-0 text-sm"
+		>
+			Privacy Policy
 		</TextButton>
 	</footer>
 );
