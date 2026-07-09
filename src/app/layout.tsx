7ec6d11d67dcb82f872f308d2b73d2df37a21094
Haaxor1689 <betkomaros@gmail.com>
2026-07-09 13:47:40 +0200

    Added canonical urls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export const metadata: Metadata = {
 	description:
 		'A tool for creating and sharing your custom World of Warcraft talent trees and builds',
 	icons: [{ rel: 'icon', url: '/icon.png' }],
-	metadataBase: new URL(env.DEPLOY_URL)
+	metadataBase: new URL(env.DEPLOY_URL),
+	alternates: {
+		canonical: './'
+	}
 };
 
 const RootLayout = async ({ children }: LayoutProps<'/'>) => (
